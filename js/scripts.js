eada338af2982374da9f7fbb989b300e9449822f
Amanda Otero <mandystar018@gmail.com>
2020-10-22 16:32:36 -0700

    Add if statement from line 28-35

--- js/scripts.js
+++ js/scripts.js
@@ -19,35 +19,22 @@ const divide = function(number1, number2) {
 // User interface logic:
 
 $(document).ready(function() {
-	$("form#add").submit(function(event) {
-		event.preventDefault();
-		const number1 = parseInt($("#add1").val());
-		const number2 = parseInt($("#add2").val());
-		const result = add(number1, number2);
-		$("#output1").text(result);
-	});
-	
-	$("form#subtract").submit(function(event){
-		event.preventDefault();
-		const number1 = parseInt($("#subtract1").val());
-		const number2 = parseInt($("#subtract2").val());
-		const result = subtract(number1, number2);
-		$("#output2").text(result);
-	});
+  $("form#calculator").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+		const operator = $("input:radio[name=operator]:checked").val();
+		let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
 
-	$("form#multiply").submit(function(event){
-		event.preventDefault();
-		const number1 = parseInt($("#multiply1").val());
-		const number2 = parseInt($("#multiply2").val());
-		const result = multiply(number1, number2);
-		$("#output3").text(result);
-	});
-
-	$("form#divide").submit(function(event){
-		event.preventDefault();
-		const number1 = parseInt($("#divide1").val());
-		const number2 = parseInt($("#divide2").val());
-		const result = divide(number1, number2);
-		$("#output4").text(result);
-	});
+  });
 });
